e4896a3cdfc2352532edbd819d4b287df81aa53c
Bryan Braun <bryan@heysparkbox.com>
2021-02-12 18:07:42 -0500

    refactor: update the button story to use the new stories format (CSF)
    See: https://storybook.js.org/docs/react/api/csf

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -2,4 +2,8 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Button } from '.';
 
-storiesOf('Button', module).add('with text', () => <Button text="I'm a Button!" />);
+export default {
+  title: 'Components/Button',
+};
+
+export const WithText = () => <Button text="I'm a Button!" />;
